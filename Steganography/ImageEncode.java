a966b4046a4abe527ad5b8825b9223590c89bf33
Soumyadeep Banerjee <87521742+SoumyadeepB2001@users.noreply.github.com>
2025-04-05 16:56:26 +0530

    Used the utility class to convert to binary string

--- Steganography/ImageEncode.java
+++ Steganography/ImageEncode.java
@@ -4,59 +4,15 @@ import javax.imageio.ImageIO;
 
 public class ImageEncode {
 
-    String msg;
-    int shift;
     File original_file;
-    String encrypted_msg;
     String binary_msg;
     File encoded_file;
 
     public ImageEncode(String message, int cipher_shift, File fi) {
-        msg = message;
-        shift = cipher_shift;
         original_file = fi;
-        encrypted_msg = encrypt();
-        binary_msg = convert_to_binary_string();
+        binary_msg = EncryptAndConvert.msg_to_binary_string(message, cipher_shift);
         System.out.println(binary_msg);
-    }
-
-    // Encrypt the message using Caesar Cipher
-    public String encrypt() {
-        StringBuilder result = new StringBuilder();
-        for (char ch : msg.toCharArray()) {
-            if (ch == 10) { // Preserve newline
-                result.append(ch);
-            } else if (ch >= 32 && ch <= 126) { // Shift within the printable ASCII range
-                int newChar = ((ch - 32 + shift) % 95) + 32;
-                result.append((char) newChar);
-            }
-        }
-        return result.toString();
-    }
-
-    // Convert encrypted message to binary string
-    public String convert_to_binary_string() {
-        StringBuilder binaryOutput = new StringBuilder();
-
-        for (char ch : encrypted_msg.toCharArray()) {
-            binaryOutput.append(String.format("%8s", Integer.toBinaryString(ch)).replace(' ', '0'));
-        }
-
-        binaryOutput.append("00000000"); // End of message marker
-
-        // Append cipher shift value and termination marker
-        // Do not forget to convert the shift from integer to String
-        // otherwise it will take the numeric value instead of the character's ASCII
-
-        String shift_string = Integer.toString(shift);
-
-        for (char ch : shift_string.toCharArray()) {
-            binaryOutput.append(String.format("%8s", Integer.toBinaryString(ch)).replace(' ', '0'));
-        }
-
-        binaryOutput.append("00000000"); // End of shift value marker
-        return binaryOutput.toString();
-    }
+    }   
 
     // Encode the binary message into the LSB of all RGB channels
     public String encode() {
